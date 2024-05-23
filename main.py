35e8d39ea8d1cc5a056a9ec495fc1fa873f6160b
Nikita_Kachur <nikitakachurua@gmail.com>
2024-05-23 00:05:30 -0400

    file which will conatin my thoughts about the project / eisenhover  object createdbut going to be changed

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello World!')
+import datetime
+import threading
+
+# Create data structure than can store my tasks +
+# Receive current time and date 
+# Show up time/ show up all  stored tasks 
+# make a simple prioritazation by assigning number for each task. Taslk with number closer to 0 is more priority. 
+
+# simple structure which acts as a "database"
+
+eisenhower_matrix = {
+    'green': ['do task that Mahamat said until Friday (included)', 'Send job fair invitation and make notes about it',
+              'Create Ukrainian chat group'],
+    'yellow': ['Do Jiu Jitsu', 'Learn data analyst course', 'Do breathing exercise', 'Apply for jobs', 'Register at "Rover",'
+               'find additional income', 'pay bills'],
+    'blue':  ['Wash clothes', 'clean apartment', 'make food',],
+    'red': []
+}
+
+print(eisenhower_matrix['green'])
