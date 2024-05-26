2453d3aed42a0598cb07392bfbbb096e335fc952
Nikita_Kachur <nikitakachurua@gmail.com>
2024-05-25 20:07:29 -0400

    function add task added

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import datetime
 import threading
 
-# Create data structure than can store my tasks +
-# Receive current time and date 
-# Show up time/ show up all  stored tasks 
-# make a simple prioritazation by assigning number for each task. Taslk with number closer to 0 is more priority. 
 
-# simple structure which acts as a "database"
 
-eisenhower_matrix = {
-    'green': ['do task that Mahamat said until Friday (included)', 'Send job fair invitation and make notes about it',
-              'Create Ukrainian chat group'],
-    'yellow': ['Do Jiu Jitsu', 'Learn data analyst course', 'Do breathing exercise', 'Apply for jobs', 'Register at "Rover",'
-               'find additional income', 'pay bills'],
-    'blue':  ['Wash clothes', 'clean apartment', 'make food',],
-    'red': []
+# 1 create a function for adding tasks to my matrix +
+# Create function protection, create testing for the function
+# Create function for deleting task, createfunction protection, create testing for function
+# Create preventing dublication function, which will ask me before adding element to list, do i really want to add this task?
+# Create function protection, testing for  the function 
+
+
+
+
+work_time = '5:00 PM'
+eisenhower_matrix_1 = {
+    'green': {
+        'work': {
+            'small': ['Job fair case notes', 'computer literacy'],
+            'medium': {'Create resumefor the client'},
+            'large': {'Enroll client process'},
+        },
+        'personal': {
+            'small': ['Register at "Rover"'],
+            'medium': ['Make PSEG payment'],
+            'large': ['Call to USCIS'],
+        }
+    }
 }
 
-print(eisenhower_matrix['green'])
+# Function for adding task to specific quadrant and task  type
+def add_task_to_matrix(task: str, task_importance: str, task_size: str, task_type: str):
+    eisenhower_matrix_1[task_importance][task_type][task_size].append(task)
+    print('Task successfully added!')
+    print(eisenhower_matrix_1)
+
+add_task_to_matrix('lol', 'green', 'small', 'personal')
