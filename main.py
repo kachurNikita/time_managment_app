034a275fa6a98d7a6ae3c48353c6df60850e1faa
Nikita_Kachur <nikitakachurua@gmail.com>
2024-06-05 18:20:09 -0400

    Task, Quadrat: classes added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from matrix_quadrat import Quadrat
 import datetime
 import threading
 
@@ -8,22 +9,54 @@ class EisenhoverMatrix:
         self.matrix = {}
             
     def show_matrix(self):
-        print(self.matrix)
+        if self.is_matrix_empty():
+            raise Exception (f'There are no quadrats in matrix')
+        else:
+            return self.matrix
+    
+    def is_matrix_empty(self):
+        return self.matrix == {}
         
     def is_quadrat_exist(self, quadrat):
         return quadrat in self.matrix.keys()
 
-    def create_quadrat(self, quadrat_name):
-        if not self.is_quadrat_exist(quadrat_name):
-            self.matrix[quadrat_name] = {}
+    def is_quadrats_limit(self):
+        iterator = 0
+        for key in self.matrix.keys():
+            iterator += 1
+        return iterator < 4
+
+    def create_quadrat(self, quadrat_type):
+        if self.is_quadrats_limit():
+            if not self.is_quadrat_exist(quadrat_type):
+                self.matrix[quadrat_type] = Quadrat()
+            else:
+                raise KeyError (f"The quadrat {quadrat_type} is already exist")
         else:
-            raise KeyError (f"The quadrat {quadrat_name} is already exist")
+            raise Exception ('No more quadrats can be added')
     
     def delete_quadrat(self, quadrat_name):
         if self.is_quadrat_exist(quadrat_name):
             self.matrix.pop(quadrat_name)
         else:
-            raise Exception (f'Quadrat {quadrat_name} is no longer exist')
+            raise Exception (f"Quadrat {quadrat_name} is not exist")
+    
+    def add_task(self, quadrat_type, task_size)
+        
+        
+matrix_1 = EisenhoverMatrix()
+matrix_1.create_quadrat('green')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ matrix_quadrat.py
@@ -0,0 +1,14 @@
+class  Quadrat:
+    def __init__(self):
+        self.quadrat = {
+            'work': {
+                'small': [],
+                'medium': [],
+                'large': []
+            },
+            'personal': {
+                'small': [],
+                'medium': [],
+                'large': []
+            }
+        }
