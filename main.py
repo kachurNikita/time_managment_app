9eb75c4293e73ed8f627ca6b5caa42a45b97eff7
Nikita_Kachur <nikitakachurua@gmail.com>
2024-08-13 14:02:19 -0400

    Implementing idea of task displaying based on day, time, and quadrats

--- main.py
+++ main.py
@@ -6,39 +6,85 @@ from time import strftime
 
 TODAY = date.today()
 WEEKDAY = date.weekday(TODAY)
+temporary_store = {}
 
 # Definition of 'Eisenhover_matrix class'
 class EisenhoverMatrix:
     
-    # Function wichallows us get a current time 
-    def get__current_time(self):
-        return strftime('%H:%M')
-        
-    # Function which allows us to get day of the week 
-    def get_weekday(self, day_id):
-        return self.sqlite_con(f"SELECT weekday FROM weekdays WHERE weekday_ID = '{day_id}'")
-    
-    # Function which allows us to retrieve the most important quadrat based on the time and the day!
-    def get_mos(self):
-        pass
     
     # Function wich allows us make connection to sqlite3 database and specific commands execution 
     def sqlite_con(self, statement):
         con = sqlite3.connect("EisenhoverMatrix.db")
         cursor = con.cursor()
-        result = cursor.execute(statement)
+        response = cursor.execute(statement)
         con.commit()
-        return result
+        return response
+    
+    # Function wich allows us get a current time 
+    def get_time(self):
+        return strftime('%H:%M')
+        
+    # Function which allows us to get day of the week 
+    def get_weekday(self, day_id):
+        response = self.sqlite_con(f"SELECT weekday FROM weekdays WHERE weekday_ID = '{day_id}'")
+        return ''.join(response.fetchone())
+    
+    def get_quadrats(self):
+        quadrats = self.sqlite_con(f"SELECT * FROM quadrat_importance").fetchall()
+        return self.get_top_quadrat(quadrats)
+    
+    # Function which allows us to retrieve the most important quadrat based on the time and the day!
+    def get_top_quadrat(self, quadrats):
+        quadrat_name, importance = quadrats[0]
+        if len(quadrats) > 1:
+            for quadrat in quadrats:
+                if quadrat[1] < importance:
+                    importance = quadrat[1]
+                    quadrat_name = quadrat[0]
+            quadrats.remove(quadrat)
+            return quadrats
+        else:
+            quadrats.remove(quadrat)
+            return quadrats
+
+        #     return self.store_temporary_here(quadrats)
+        #     # return quadrat_name
+        # else:
+        #     quadrats.remove(quadrat)
+        #     return self.store_temporary_here(quadrats)
+            # return quadrat_name
+    
+    # Testing fnction
+    def store_temporary_here(self, quadrat_name, importance):
+        temporary_store[quadrat_name] = importance
+        print(temporary_store)
+        return temporary_store
+
+    # Function which allows us to check is there is a weekend
+    def is_today_weekend(self, weekday):
+        weekday = self.return_day(weekday).lower()
+        if weekday  == 'sunday' or weekday == 'saturday':
+            return True
+        else:
+            return False
+    
+    # #  Function which allows us to identify which task group to chose 
+    # def task_group(self, is_today_weekend, importance, quadrats, quadrat_name):
+    #     if is_today_weekend:
+    #         result = self.sqlite_con(f"SELECT task FROM {quadrat_name} WHERE task_group = 'work'").fetchall()
+    #         return result
+        # else
+# -------------------------------------------------------------------
     
     # Executes only ones and will create table for storing "qudrat importance" (have to be optimized )
-    def create_quadrat_importance_table(self):  
-        self.sqlite_con(f"CREATE TABLE quadrat_importance('quadrat name', 'quadrat importance')")   
+    # def create_quadrat_importance_table(self):  
+    #     self.sqlite_con(f"CREATE TABLE quadrat_importance('quadrat_name', 'importance')")   
     
     # Function which allows us assign quadrat's importance
-    def assign_quadrat_importance(self, quadrat_name, importance):
-        self.sqlite_con(f'''INSERT INTO quadrat_importance('quadrat name', 'quadrat importance') 
-                        VALUES('{quadrat_name}', '{importance}')
-                        ''')
+    # def assign_quadrat_importance(self, quadrat_name, importance):
+    #     self.sqlite_con(f'''INSERT INTO quadrat_importance('quadrat_name', 'importance') 
+    #                     VALUES('{quadrat_name}', '{importance}')
+    #                     ''')
     
     # Function which creates a weekdays table (have to be optimized ) 
     def create_weekdays_table(self):
@@ -50,7 +96,7 @@ class EisenhoverMatrix:
             self.sqlite_con(f'''INSERT INTO weekdays('weekday_ID', 'weekday')
                             VALUES('{key}', '{str(weekdays[key])}')
                             ''')
-        
+            
      # Function wich returns the weekday
     def return_day(self, day_id):
          weekday = self.sqlite_con(f"SELECT weekday FROM weekdays WHERE weekday_ID = '{day_id}'")
@@ -72,7 +118,7 @@ class EisenhoverMatrix:
     # Function which allows to check how many tables (quadrats) at database (MAX 4 allowed)
     def is_quadrats_limit(self):
         quadrat_counter = 0
-        quadrat_list_query_response = self.sqlite_con("SELECT name FROM sqlite_master WHERE type='table' ORDER BY Name")
+        quadrat_list_query_response = self.sqlite_con("SELECT 'quadrat name' FROM quadrat_importance")
         for quadrat in quadrat_list_query_response.fetchall():
             quadrat_counter += 1
         return quadrat_counter < 4
@@ -87,8 +133,9 @@ class EisenhoverMatrix:
     def create_quadrat(self, quadrat_name, importance):
         if  self.is_quadrats_limit():
             if not self.is_quadrat_exist(quadrat_name):
-                self.sqlite_con(f"CREATE TABLE {quadrat_name}('task', 'group', 'date', 'weekday')")
-                self.assign_quadrat_importance(quadrat_name, importance)
+                self.sqlite_con(f"CREATE TABLE {quadrat_name}('task', 'task_group', 'date', 'weekday')")
+                self.store_temporary_here(quadrat_name, importance)
+                # self.assign_quadrat_importance(quadrat_name, importance)
                 print(f'Quadrat {quadrat_name} is successfully created!')
             else:
                 raise Exception(f'Quadrat with name {quadrat_name} is already exist!')
@@ -99,7 +146,7 @@ class EisenhoverMatrix:
     def delete_quadrat(self, quadrat_name):
         if self.is_quadrat_exist(quadrat_name):
             self.sqlite_con(f"DROP TABLE IF EXISTS {quadrat_name}")
-            self.sqlite_con(f'''DELETE FROM  quadrat_importance WHERE "quadrat name"  = "{quadrat_name}"''')
+            self.sqlite_con(f'''DELETE FROM  quadrat_importance WHERE quadrat_name  = "{quadrat_name}"''')
             print(f'Quadrat {quadrat_name} successfully deleted!')
         else:
             print(f'Quadrat with name {quadrat_name} is not exist!')
@@ -109,8 +156,8 @@ class EisenhoverMatrix:
         if self.is_quadrat_exist(quadrat_name):
             if not self.is_task_exist(quadrat_name, task):
                 self.sqlite_con(f'''
-                                   INSERT INTO {quadrat_name}('task', 'group', 'date', 'weekday')
-                                   VALUES('{task}', '{task_type}', '{today}', '{self.return_day(weekday)}')
+                                   INSERT INTO {quadrat_name}('task', 'task_group', 'date', 'weekday')
+                                   VALUES('{task}', '{task_type}', '{TODAY}', '{self.return_day(WEEKDAY)}')
                                    ''')
                 print('Values is added')
             else:
@@ -130,5 +177,41 @@ class EisenhoverMatrix:
             raise Exception(f'Quadrat with name {quadrat_name} is not exist!')
         
 matrix = EisenhoverMatrix()
-matrix.create_quadrat('imp', 1)
 
+# matrix.get_top_quadrat()
+matrix.create_quadrat('urgent', 1)
+# matrix.delete_quadrat('important')
+# matrix.add_task('urgent', 'Call client ', 'work')
+# print(matrix.get_quadrats())
+
+
+
+
+
+
+
+# Make it work
+#  Implement it !
+
+
+
+
+
+
+
+
+# Instead of of tables limitation, do limitation at important table +
+# Create function which will check if current quadrat is empty, it will go to another one until finds not empty 
+# Create function is working time , returns true or false
+# Create Function which will check is there is any tasks left from previous day, if yes, just substitute day 
+
+# Get Quadrats  +
+# Get most important quadrat - Create new function for sorting quadrats - received quadrats from and returning the top one +
+# Store the rest of quadrats somewhere 
+# Get current time 
+# Get current day 
+# Sort task by day
+# Sort task by time 
+
+# Get all quadrats
+# Get the day, becasue if its weekend,
