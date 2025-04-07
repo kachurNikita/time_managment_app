a23da2d2906a3e8022010e9895ddf26cbe217fd9
Nikita_Kachur <nikitakachurua@gmail.com>
2025-04-07 16:09:23 -0400

    access key removed from file

--- chat_gtp_API.py
+++ chat_gtp_API.py
@@ -4,7 +4,7 @@ import requests
 import openai
 
 # Access key which is generated when account created 
-OPENAI_API_KEY = 'sk-6DeOQTpymgZ-Uw5HoqHrQDbqo2FBQm9n99fD2T2Ln7T3BlbkFJWNs9BEEArTvWW37HMljEUBwECDZlcdYbZFIO5Oq6IA'
+
 
 # OpenAI instance
 client = OpenAI(api_key=OPENAI_API_KEY)
